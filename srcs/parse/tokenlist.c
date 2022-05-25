b3050c7fd107b632ad488cb3c7a83dd74b953bea
dino9881 <76129597+dino9881@users.noreply.github.com>
2022-05-25 17:06:59 +0900

    [FIX] : fix tokenize func (#16)

--- srcs/parse/tokenlist.c
+++ srcs/parse/tokenlist.c
@@ -3,14 +3,17 @@
 t_token_list *new_token_node(char *tok)
 {
 	t_token_list	*new_token;
+	int	toklen;
 
 	new_token = malloc(sizeof(t_token_list));
 	if (!new_token)
 		return (NULL);
-	new_token->token.str = tok; 
-	if (tok == "<<" || tok == "<" || tok == ">>" || tok == ">")
+	new_token->token.str = tok;
+	toklen = ft_strlen(tok); 
+	if (!ft_strncmp(tok, "<", toklen) || !ft_strncmp(tok, "<<", toklen)\
+	 ||!ft_strncmp(tok, ">>", toklen) || !ft_strncmp(tok, ">", toklen))
 		new_token->token.type = REDIRECT;
-	else if (tok == "|")
+	else if (!ft_strncmp(tok, "|", toklen))
 		new_token->token.type = PIPE;
 	else
 		new_token->token.type = WORD;
@@ -25,6 +28,7 @@ t_token_list *make_token_list(char **tokens)
 	t_token_list	*cur;
 	t_token_list	*new_token;
 
+	start = ft_calloc(1, sizeof(t_token_list));
 	cur = start;
 	i = -1;
 	while (tokens[++i])
