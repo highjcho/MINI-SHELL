1a6387308de68d90ef42c35f86bc6771ce2c567a
highcho <highjcho@gmail.com>
2022-05-31 17:29:50 +0900

    [FEAT]: create env & free utils

---
+++ srcs/utils/free.c
@@ -0,0 +1,31 @@
+#include "../../includes/utils.h"
+
+void	free_env(t_env *env)
+{
+	t_env_node	*cur;
+	t_env_node	*tmp;
+
+	cur = env->h_node.next;
+	while (cur)
+	{
+		tmp = cur->next;
+		free(cur->key);
+		if (cur->e_flag)
+		{
+			free(cur->value);
+			free(cur->export);
+		}
+		free(cur);
+		cur = tmp;
+	}
+}
+
+void	double_free(char **str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		free(str[i]);
+	free(str);
+}
