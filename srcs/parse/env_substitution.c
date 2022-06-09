5676d47e91a844e81d34c84112aacb67d33bb8fd
Jongho Kim <whdghthdms44@gmail.com>
2022-06-09 19:43:15 +0900

    [FIX] : env sub fix

--- srcs/parse/env_substitution.c
+++ srcs/parse/env_substitution.c
@@ -6,7 +6,7 @@
 /*   By: jonkim <jonkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:49:37 by jonkim            #+#    #+#             */
-/*   Updated: 2022/06/09 13:57:21 by jonkim           ###   ########.fr       */
+/*   Updated: 2022/06/09 19:29:04 by jonkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ char	*ret_sub(char *str, char *sub, int pos1, int pos2)
 	ret = ft_calloc(ft_strlen(str) - pos2 + pos1 + ft_strlen(sub) + 1, 1);
 	if (!sub)
 		sub = ft_strdup("");
+	sub = ft_strdup(sub);
 	if (!ret)
 		return (NULL);
 	while (str[++i])
 	{
 		if (i <= pos2 && i >= pos1)
-		{
 			while (sub[j])
 				ret[idx++] = sub[j++];
-		}
 		else
 			ret[idx++] = str[i];
 	}
+	free(sub);
 	free(str);
 	return (ret);
 }
@@ -46,38 +46,59 @@ char	*substitution(char *str, int pos, t_env *env)
 	int		tmp;
 	char	*sub;
 
+	sub = NULL;
 	tmp = pos + 1;
 	pos++;
 	if (str[pos] == '?')
-	pos++;
+	{
+		pos++;
+		sub = get_env_value(env, ft_substr(str, tmp, pos - tmp));
+		return (ret_sub(str, sub, tmp - 1, pos - 1));
+	}
+	if (!ft_isalpha(str[pos]) && str[pos] != '_' && str[pos])
+	{
+		sub = get_env_value(env, ft_substr(str, tmp, pos + 1 - tmp));
+		return (ret_sub(str, sub, tmp - 1, pos));
+	}
 	while (ft_isalnum(str[pos]) || str[pos] == '_')
 		pos++;
 	sub = get_env_value(env, ft_substr(str, tmp, pos - tmp));
 	return (ret_sub(str, sub, tmp - 1, pos - 1));
 }
 
+void	single_quote_check(char *str, int *i, int *tmp)
+{
+	*tmp = *i;
+	while (str[++(*tmp)])
+	{
+		if (str[*tmp] == str[*i])
+			*i = *tmp;
+	}
+}
+
 char	*env_check(char *str, t_env *env)
 {
 	int	i;
+	int	j;
 	int	tmp;
 
 	i = -1;
+	j = 0;
 	tmp = 0;
 	while (str[++i])
 	{
 		if (str[i] == '\'')
-		{
-			tmp = i;
-			while (str[++tmp])
-			{
-				if (str[tmp] == str[i])
-					i = tmp;
-			}
-		}
+			single_quote_check(str, &i, &tmp);
 		if (str[i] == '$')
 		{
+			if (str[i + 1] == '$' || str[i + 1] == 0)
+			{
+				j++;
+				i = j - 1;
+				continue;
+			}
 			str = substitution(str, i, env);
-			i = -1;
+			i = j - 1;
 		}
 	}
 	return (str);
